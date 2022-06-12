111eba7020c2c94e17d4e55d1740efd63acaf5ee
Claudio-Solar <cv.solar16@gmail.com>
2022-06-12 17:41:55 -0500

    Cambio1
    Agregue Java

--- JavaSesion2/src/main/java/com/mycompany/javasesion2/JavaSesion2.java
+++ JavaSesion2/src/main/java/com/mycompany/javasesion2/JavaSesion2.java
@@ -13,5 +13,8 @@ public class JavaSesion2 {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        
+        System.out.println("Hello Java");
+        
     }
 }
